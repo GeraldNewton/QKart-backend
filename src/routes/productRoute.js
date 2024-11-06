4d4d6869f8c401f9412e7b83d15b411f1df9612c
GeraldNewton <arpitnewton1@gmail.com>
2024-11-06 11:49:50 +0530

    work commit

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,9 +1,8 @@
 const express=require("express")
 const router=express.Router();
 const {get_products_by_category,get_all_products}=require("../controllers/product_controller.js")
-const verifyToken=require("../middleware/verify_token.js")
 
-router.get("/",verifyToken,get_all_products);
-router.get("/:category",verifyToken,get_products_by_category);
+router.get("/",get_all_products);
+router.get("/:category",get_products_by_category);
 
 module.exports=router
